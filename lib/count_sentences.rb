18656f27409a32c7aa09ecf13584d9dd90aae78f
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-08-24 01:18:03 +0300

    commit tree 8561c58476baef82a49d72f6b09a716ac652391e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
+    end_with?('.')
     
   end
 
   def question?
+    end_with?('?')
 
   end
 
   def exclamation?
+    end_with?('!')
 
   end
 
-  def count_sentences
-
+  def count_sentences 
+    split(/\?|\.|!/).filter{|w|w!=""}.size
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
